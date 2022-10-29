f49197323ff996fba3c12c4a6db7cdc5a8a16467
WeslleiBrito <wesllei.wbs@gmail.com>
2022-10-29 17:47:02 -0300

    inicializando repositório

---
+++ script.js
@@ -0,0 +1,29 @@
+// semana 2
+
+const cursos = ["Python", "JavaScript", "APIs REST"]
+const turmas = ["Falcões", "Touros", "Lobos", "Carcarais", "Ursos"]
+const estudantes = ["Alice", "Jonas", "Verônica", "Marcele", "Mel"]
+
+// semana 3
+
+const meusCursos = [
+    {
+        nome: "Python",
+        descricao: "Muito utilizada da análise de dados, inteligencia artificial entre outras",
+        duracao: "1 Ano",
+        valor: 2.000
+    },
+    {
+        nome: "JavaScript",
+        descricao: "A queidinha do back-end",
+        duracao: "1 ano e 6 meses",
+        valor: 12.000
+    },
+    {
+        nome: "APIs Rest",
+        descricao: "Cada um com sua responsabilidade",
+        duracao: "3 meses",
+        valor: 750.00
+    }
+]
+
